63988f92363766972154afa6cbaa3eed1c3d016d
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-01-03 23:13:19 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import {HomeComponent} from './coponents/home/home.component';
 
 
 const routes: Routes = [
+  {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home' , component: HomeComponent}

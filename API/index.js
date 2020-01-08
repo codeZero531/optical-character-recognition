6fe44b804e483c72d09f74d90f86c0cab2884740
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-01-09 01:51:13 +0530

    Update index.js

--- API/index.js
+++ API/index.js
@@ -8,12 +8,16 @@ const createWorker = require('tesseract.js');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit:'100mb',  extended: true }));
+app.use(express.urlencoded({limit:'100mb',  extended: true }));
 app.use(cors());
 
+app.use(bodyParser.json({ limit: '100mb', extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
 
-const PORT = process.env.PORT ||  3000
+
+
+const PORT = process.env.PORT ||  3000 ;
 
 
 const storage = multer.diskStorage({
@@ -84,6 +88,18 @@ app.post('/ruwan', (req, res) => {
    res.json({
       message: 'done'
    });
+});
+
+app.post('/image' , (req, res) => {
+    console.log('done');
+    console.log(req.body);
+
+    
+
+    res.status(200).json({
+        message : 'hello'
+    });
+
 });
 
 app.use(morgan());

e0aaf00937efac1b1889f5fbafd071940baaf029
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-01-10 00:01:03 +0530

    Update main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 import 'package:image_picker/image_picker.dart';
 
 void main() {

7af9de8aae1cda64b1e749e2288345b5c7f0349c
Janaka Edirisinghe <janakapradeepedirisinghe@gmail.com>
2020-01-10 01:01:51 +0530

    code reconstruct

--- API/index.js
+++ API/index.js
@@ -7,111 +7,46 @@ const createWorker = require('tesseract.js');
 const fs = require('fs');
 
 
-
 const app = express();
 
-app.use(express.json({limit:'100mb',  extended: true }));
-app.use(express.urlencoded({limit:'100mb',  extended: true }));
+app.use(express.json({limit: '100mb', extended: true}));
+app.use(express.urlencoded({limit: '100mb', extended: true}));
 app.use(cors());
 
-app.use(bodyParser.json({ limit: '100mb', extended: true }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }));
+app.use(bodyParser.json({limit: '100mb', extended: true}));
+app.use(bodyParser.urlencoded({extended: true, limit: '100mb'}));
 
+const PORT = process.env.PORT || 3000;
 
-
-const PORT = process.env.PORT ||  3000 ;
-
-
-const storage = multer.diskStorage({
-    destination: (req, file, callBack) => {
-        callBack(null, 'uploads')
-    } ,
-    filename: (Req, file, callBack) => {
-        callBack(null, `${file.originalname}`);
-    }
-});
-
-var upload = multer({storage: storage});
-
-app.post('/file', upload.single('file'), (req, res, next) => {
-    const file = req.file;
-    const language = req.body.language;
-
-    console.log(req.body.language);
-    // var text = test();
-
-    if (!file) {
-        const  error = new Error('Please upload the file');
-        error.statusCode = 400;
-        return next(error);
-    }
-
-    // ocr function
-
-    const worker = createWorker.createWorker({
-        logger: m => console.log((m.progress)*100 + "%"), // Add logger here
-    });
-    (async () => {
-        await worker.load();
-        await worker.loadLanguage(language);
-        await worker.initialize(language);
-        const { data: { text } } = await worker.recognize(`./uploads/${file.originalname}`);
-        console.log(text);
-        await worker.terminate();
-        await res.json({
-            text: text,
-        });
-
-    })();
-
-});
-
-//multiple files
-app.post('/multipleFiles', upload.array('files'), (req, res, next) => {
-    const files = req.files;
-    console.log(files);
-    if (!files) {
-        const  error = new Error('Please upload the file');
-        error.statusCode = 400;
-        return next(error);
-    }
-    res.send({status: 'ok'});
-});
-
-//api test
-app.get('/test', (req, res) => {
-   res.status(200).json({
-       message: 'test work!'
-   });
-});
-
-app.post('/ruwan', (req, res) => {
-   console.log(req.body);
-   res.json({
-      message: 'done'
-   });
-});
-
-app.post('/image' , (req, res) => {
-    fs.writeFile(`uploads/${req.body.name}`, req.body.image, {encoding: 'base64'}, function(err) {
+app.post('/image', (req, res) => {
+    fs.writeFile(`uploads/${req.body.name}`, req.body.image, {encoding: 'base64'}, function (err) {
         console.log('File created');
     });
-    console.log(req.body.image);
+
+    var selectedLanguage;
+
+    if (req.body.language) {
+        selectedLanguage = req.body.language;
+    } else {
+        selectedLanguage = 'eng';
+    }
+    console.log(selectedLanguage);
 
     // ocr function
-
     const workerMobile = createWorker.createWorker({
-        logger: m => console.log(((m.progress)*100) + "%"), // Add logger here
+        logger: m => console.log(((m.progress) * 100) + "%"), // Add logger here
     });
 
     (async () => {
         await workerMobile.load();
-        await workerMobile.loadLanguage('eng');
-        await workerMobile.initialize('eng');
-        const { data: { text } } = await workerMobile.recognize(`./uploads/${req.body.name}`);
+        await workerMobile.loadLanguage(selectedLanguage);
+        await workerMobile.initialize(selectedLanguage);
+        const {data: {text}} = await workerMobile.recognize(`./uploads/${req.body.name}`);
         console.log(text);
         await workerMobile.terminate();
-        await res.status(200).send(text);
+        await res.status(200).json({
+            word: text
+        });
 
     })();
 

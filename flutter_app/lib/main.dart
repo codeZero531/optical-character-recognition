bf4dd2da39a898764c8902d95c699bc64bf014dc
ruwanbandara <ruwan.se.uok@gmail.com>
2020-01-09 19:59:57 +0530

    Image Upload, Show Image function added, Make Post request

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -22,13 +22,93 @@ class MyApp extends StatelessWidget {
 class UploadImage extends StatefulWidget {
   UploadImage() : super();
 
-  final String title = "Upload Image Demo";
+  final String title = "Upload Image ";
 
   @override
   UploadImageState createState() => UploadImageState();
 }
 
 class UploadImageState extends State<UploadImage> {
+ 
+ static final String uploadEndPoint = 'http://localhost:3000/image';
+  Future<File> file;
+  String status = '';
+  String base64Image;
+  File tmpFile;
+  String errMessage = 'Error Uploading Image';
+
+  chooseImage() {
+    setState(() {
+      file = ImagePicker.pickImage(source: ImageSource.gallery);
+    });
+    setStatus('');
+  }
+
+  setStatus(String message) {
+    setState(() {
+      status = message;
+    });
+  }
+
+  startUpload() {
+    setStatus('Uploading Image...');
+    if (null == tmpFile) {
+      setStatus(errMessage);
+      return;
+    }
+    String fileName = tmpFile.path.split('/').last;
+    upload(fileName);
+  }
+
+  upload(String fileName) async {
+    // set up POST request
+    String url = 'http://10.0.2.2:3000/image';
+    Map<String, String> headers = {"Content-type": "application/json"};
+
+    // make POST request
+    Response response = await post(url, body: {
+      "image": base64Image,
+      "name": fileName,
+    });
+
+    String text = response.body;
+    setStatus(text);
+  }
+
+  Widget showImage() {
+    return FutureBuilder<File>(
+      future: file,
+      builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            null != snapshot.data) {
+          tmpFile = snapshot.data;
+          base64Image = base64Encode(snapshot.data.readAsBytesSync());
+          return Flexible(
+            child: Image.file(
+              snapshot.data,
+              fit: BoxFit.fill,
+            ),
+          );
+        } else if (null != snapshot.error) {
+          return const Text(
+            'Error Picking Image',
+            textAlign: TextAlign.center,
+          );
+        } else {
+          return const Text(
+            'No Image Selected',
+            textAlign: TextAlign.center,
+          );
+        }
+      },
+    );
+  }
+
+ 
+ 
+ 
+ 
+ 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build

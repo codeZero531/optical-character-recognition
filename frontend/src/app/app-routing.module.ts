518c2d688adb806b3ad10c41704d426b8a1cd30c
ruwanbandara <ruwan.se.uok@gmail.com>
2020-01-12 13:03:26 +0530

    Update app-routing.module.ts

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import {LoginComponent} from './coponents/login/login.component';
 import {RegisterComponent} from './coponents/register/register.component';
 import {HomeComponent} from './coponents/home/home.component';
 import {AuthGuard} from './guards/auth.guard';
+import {PaswordResetComponent} from './coponents/pasword-reset/pasword-reset.component';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path: 'home' , component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'password-reset', component: PaswordResetComponent},
   {path: '', component: LoginComponent},
 ];
 
